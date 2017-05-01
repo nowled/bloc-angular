2529d27f130bab2e2b62249877586bb03eaec282
Charles Peterson <nowledster@gmail.com>
2017-05-01 11:05:03 +0300

    Checkpoint3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,26 @@
 (function() {
-    function config($stateProvider, $locationProvider) {
-        //Disables the hashbang URLs
-        $locationProvider
-            .html5Mode({
-                enabled: true,
-                requireBase: false
-            });
-        $stateProvider
-            .state('landing', {
-                url: '/',
-                templateUrl: '/templates/landing.html'
-            })
+    function config($stateProvider, $locationProvider) {}
+    //Disable Hashbang Mode
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-           .state('album', {
-               url: '/album',
-               templateUrl: '/templates/album.html'
-     })
-            .state('collection', {
-                url: '/collection',
-                templateUrl: '/templates/collection.html'
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateUrl: '/templates/landing.html'
+        })
+        .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
 
-            });
-    }
-
-
+        })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+        });
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
