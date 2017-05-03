74117d2adc6fed3a1ed82a4aac508888eb6e98aa
Charles Peterson <nowledster@gmail.com>
2017-05-03 11:09:54 +0300

    Checkpoint5 Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@
     $stateProvider
         .state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
         })
         .state('album', {
@@ -20,6 +21,7 @@
         })
         .state('collection', {
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl: '/templates/collection.html'
         });
       }
